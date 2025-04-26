7768523bfc4e102e9c37d2579c08bb02c5bc8aa5
StillMix <stillmixplaygames@gmail.com>
2025-04-26 15:17:53 +0300

    api

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,3 +4,26 @@ export { courseApi };
 
 // Базовые константы для API
 export const API_BASE_URL = "http://localhost:8000";
+
+// GET /api/courses/ получить все курсы
+// GET /api/courses/{id} получить конкретный
+
+// POST /api/courses/ создание курса
+// DELETE /api/courses/{id} удаление курса
+// PUT /api/courses/{id} редактирование курса
+
+// POST /api/courses/{id}/info/ создание блока информации
+// DELETE /api/courses/{id}/info/{id info} удаление блока информации
+// PUT /api/courses/{id}/info/{id info} редактирование блока информации
+
+// POST /api/courses/{id}/sections/ создание раздела
+// DELETE /api/courses/{id}/sections/{id sections} удаление раздела
+// PUT /api/courses/{id}/sections/{id sections} редактирование раздела
+
+// POST /api/courses/{id}/sections/{id sections}/content создание урока
+// DELETE /api/courses/{id}/sections/{id sections}/content удаление урока
+// PUT /api/courses/{id}/sections/{id sections}/content редактирование урока
+
+// POST /api/courses/{id}/sections/{id sections}/content/{id content} создание описания урока
+// DELETE /api/courses/{id}/sections/{id sections}/content/{id content} удаление описания урока
+// PUT /api/courses/{id}/sections/{id sections}/content/{id content} редактирование описания урока
