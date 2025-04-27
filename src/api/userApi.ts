35bd467a0591bdef2e36e12f670d300077550917
StillMix <stillmixplaygames@gmail.com>
2025-04-28 01:57:41 +0300

    userApi

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
-import {
-  CourseItem,
-  CourseInfoItem,
-  CourseItemCourse,
-  CourseItemCourseContent,
-} from "@/types";
+import { UserData } from "@/types";
 const API_BASE_URL = "http://localhost:8000";
 const API_URL = `${API_BASE_URL}/api/courses`;
+
+export const createUser = async (
+  courseData: Partial<UserData>
+): Promise<UserData> => {
+  try {
+    const response = await axios.post(API_URL, courseData);
+
+    const createUser = response.data;
+    return createUser;
+  } catch (error) {
+    console.error("Ошибка при создании пользователя:", error);
+    throw error;
+  }
+};
